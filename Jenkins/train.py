738ede9ada47d35a91cb6dce11bbd3e75ce53190
JavierRodriguezFontiveros <javierrodriguezfontiveros@gmail.com>
2025-01-30 15:48:45 +0100

    jenkins

--- Jenkins/train.py
+++ Jenkins/train.py
@@ -7,10 +7,10 @@ import joblib
 data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 
-# Entrenar modelo
+#Entrenar modelo
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-# Guardar modelo entrenado
+#Guardar modelo entrenado
 joblib.dump(model, 'model.pkl')
 print("Modelo entrenado y guardado.")
